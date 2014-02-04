e7d55fd8d8bb1b5f0acddfe80f37a3e921ed4ec1
t.hayashi <t.hayashi0077+bitbucket@gmail.com>
2014-02-05 01:55:54 +0900

    Initial Commit

---
+++ ORMapper/ORAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ORAppDelegate.h
+//  ORMapper
+//
+//  Created by 林 達也 on 2014/02/05.
+//  Copyright (c) 2014年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ORMapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ORMapper
+//
+//  Created by 林 達也 on 2014/02/05.
+//  Copyright (c) 2014年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ORAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ORAppDelegate class]));
+    }
+}
